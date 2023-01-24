2d61f7a0ab442cf2a5093dd439db6c941402f259
GiselleBarbosa <g88barbosa@gmail.com>
2023-01-24 12:36:13 -0300

    iniciadas as operaçoes

--- 03-projeto-calculadora/js/script.js
+++ 03-projeto-calculadora/js/script.js
@@ -6,6 +6,11 @@ class Calculator {
     this.reset = 0;
   }
 
+  clearValues() {
+    this.upperValue.textContent = '0';
+    this.resultValue.textContent = '0';
+  }
+
   checkLastDigit(input, upperValue, reg) {
 
     if ((
@@ -19,22 +24,74 @@ class Calculator {
     }
   }
 
+  // resolve a operação
+  resolution() {
+
+    // transforma uma string em um array
+    let upperValueArray = (this.upperValue.textContent).split(" ");
+    // resultado da operação
+    let result = 0;
+
+    for (let i = 0; i <= upperValueArray.length; i++) {
+
+      let actualItem = upperValueArray[i];
+
+      if (actualItem == "+") {
+        result = parseFloat(upperValueArray[i - 1]) + parseFloat(upperValueArray[i + 1]);
+      }
+
+    }
+
+    if (result) {
+      calc.reset = 1;
+    }
+
+    this.upperValue.textContent = result;
+    this.resultValue.textContent = result;
+
+  }
+
   btnPress() {
 
     let input = this.textContent;
     let upperValue = calc.upperValue.textContent;
-    // valid only nums
+    // verifica se tem somente numeros
     var reg = new RegExp('^\\d+$');
 
-    // check digit
-    if (calc.checkLastDigit(input, upperValue, reg)) {
-      return false;
+    //se precisar resetar, limpa o display
+    if (calc.reset && reg.test(input)) {
+      upperValue = '0';
     }
 
-    if (upperValue == "0") {
-      calc.upperValue.textContent = input;
+    // limpa prop de reset
+    calc.reset = 0;
+
+    // ativa metodo de limpar display
+    if (input == 'AC') {
+
+      calc.clearValues();
+
+    } else if (input == '=') {
+
+      calc.resolution();
+
     } else {
-      calc.upperValue.textContent += input;
+
+      // checa se precisa adicionar ou não
+      if (calc.checkLastDigit(input, upperValue, reg)) {
+        return false;
+      }
+
+      // adiciona espaços aos operadores
+      if (!reg.test(input)) {
+        input = ` ${ input } `;
+      }
+
+      if (upperValue == "0") {
+        calc.upperValue.textContent = input;
+      } else {
+        calc.upperValue.textContent += input;
+      }
     }
   }
 }
